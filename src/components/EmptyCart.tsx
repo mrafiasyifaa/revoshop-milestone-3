1cf3106dfe739b4f808ddbdad55e7018c0011733
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-02-14 00:01:20 +0700

    fix: Minor fix in empty cart

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -7,14 +7,11 @@ export default function EmptyCart() {
     <div className="py-10 md:py-20 bg-linear-to-b from-blue-50 to-white flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-xl p-8 max-w-md w-full space-y-8">
         <div className="relative w-48 h-48 mx-auto">
-          <div className="absolute -top-4 -right-4 bg-blue-500 rounded-full p-2">
-            <ShoppingCart size={24} className="text-white" />
-          </div>
         </div>
 
         <div className="text-center space-y-4">
           <h2 className="text-3xl font-bold text-gray-800">
-            Your cart is feeling lonely
+            Your Cart is Empty!
           </h2>
           <p className="text-gray-600">
             It looks like you haven&apos;t added anything to your cart yet.
@@ -25,7 +22,7 @@ export default function EmptyCart() {
         <div>
           <Link
             href="/"
-            className="block bg-neutral-100 border border-darkColor/20 text-center py-2.5 rounded-full text-sm font-semibold tracking-wide hover:border-darkColor hover:bg-darkColor hover:text-white hoverEffect"
+            className="block bg-revoshop-accent border border-darkColor/20 text-center py-2.5 rounded-full text-sm text-white font-semibold tracking-wide hover:border-darkColor hover:bg-revoshop-accent-hover hover:text-white hoverEffect"
           >
             Discover Our Curated Products!
           </Link>
